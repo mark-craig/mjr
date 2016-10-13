b758208db328b9802f8b9981b5a8cd8b781d580a
Mark Craig <mcraig@berkeley.edu>
2016-10-13 01:31:06 -0700

    Added CImg

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
-#include "vector3d.h"
-#include "light.h"
-#include <iostream>
+#include "CImg.h"
 
-using namespace std;
+using namespace cimg_library;
 
 int main() {
-	Vector3D v = Vector3D(1.0, 2.0, 3.0);
-	Light l = Light();
-	cout << "success" << endl;
+  CImg<float> image("../img/lena.jpg"), visu(500,400,1,3,0);
+  const float red[] = { 255,0,0 }, green[] = { 0,255,0 }, blue[] = { 0,0,255 };
+  image.blur(2.5);
+  CImgDisplay main_disp(image,"Click a point"), draw_disp(visu,"Intensity profile");
+  while (!main_disp.is_closed() && !draw_disp.is_closed()) {
+    main_disp.wait();
+    if (main_disp.button() && main_disp.mouse_y()>=0) {
+      const int y = main_disp.mouse_y();
+      visu.fill(0).draw_graph(image.get_crop(0,y,0,0,image.width()-1,y,0,0),red,1,1,0,255,0);
+      visu.draw_graph(image.get_crop(0,y,0,1,image.width()-1,y,0,1),green,1,1,0,255,0);
+      visu.draw_graph(image.get_crop(0,y,0,2,image.width()-1,y,0,2),blue,1,1,0,255,0).display(draw_disp);
+      }
+    }
+  return 0;
 }
